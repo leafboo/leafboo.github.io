5ba5ad5e9cc8107d62dbc1bed00e79ede16c1b8a
Jan Romel <janjancaranguian@gmail.com>
2026-05-25 17:35:01 +0800

    act doesn't work so I'll test it here

--- random_stuff/parse_repos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Step 1: get each repositories in the repositories array in the YAML file repos.yaml
-readarray -t arr < <(yq '.repositories[]' repos.yaml)
-echo "${arr[0]}"
-
-# Step 2: loop through the array of repos and download the README from the repository
